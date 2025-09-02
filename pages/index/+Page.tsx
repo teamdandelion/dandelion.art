f5a470da653ca12e25b2f2a4b5b6d09ae687a23c
Dandelion Mané <dandelion@dandelion.art>
2025-09-02 15:07:32 -0700

    qql: add links, fix year

--- pages/index/+Page.tsx
+++ pages/index/+Page.tsx
@@ -20,7 +20,11 @@ export default function Page() {
             className="w-full h-auto rounded-lg shadow-lg"
           />
           <p className="text-sm text-center mt-2 text-base-content/70">
-            QQL #276, by Tyler Hobbs and Dandelion Mané (2021)
+            <a href="https://qql.art/token/0x0b62e5caff1b4f9605e78c3ca8016c0a4b4e23cbdc7209719f18ffff141120ce" className="link link-primary">
+              QQL #276
+            </a>
+            , by <a href="https://www.tylerxhobbs.com" className="link link-primary">Tyler Hobbs</a> and
+            Dandelion Mané (2022)
           </p>
           <p className="text-xs text-center mt-0 text-base-content/70 italic">
             Seed discovered by Michael Connery (2023)
